e2a1fff33cf7f63b4a880047e3fe8aa36a861cea
youssefzaguia <youssefultras02tn@gmail.com>
2023-11-26 18:04:45 +0100

    putcharfc

--- _putchar.c
+++ _putchar.c
@@ -1,8 +1,11 @@
 #include "main.h"
+
 /**
- * _putchar - Print char
- * @c: char
- * Return: size
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
 {
