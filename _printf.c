2afde4d1a6b1bf9c60f5bc13ea303b3747295fa4
youssefzaguia <youssefultras02tn@gmail.com>
2023-11-26 18:01:58 +0100

    _prinfc

--- _printf.c
+++ _printf.c
@@ -1,30 +1,75 @@
 #include "main.h"
 /**
- * _printf - Entry point
- * @format: char
- * Return:size
+ * print_all - chose the right function
+ * @ch: character
+ * Return: 0
+ */
+int (*print_all(const char ch))(va_list)
+{
+    int kk=0;
+
+    list_t printf[] = {
+        {'c', print_character},
+        {'s', print_string},
+        {'d', print_integer},
+        {'i', print_integer},
+        {'\0', NULL}};
+
+    while (printf[kk].specifiers != '\0')
+    {
+        if (ch == (printf[kk].specifiers))
+        {
+            return (printf[kk].f);
+        }
+        kk++;
+    }
+    return (0);
+}
+
+/**
+ * _printf - printf function that print character, integer and string
+ * @format: variable
+ * Return: -1,j,0
  */
 int _printf(const char *format, ...)
 {
-	int i = 0, size = 0;
-	va_list args;
+    va_list ap;
+    unsigned int i = 0, j = 0;
 
-	va_start(args, format);
-	if (!format || (format[0] == '%' && format[1] == '\0'))
-		return (-1);
-	for (i = 0; format[i] != '\0'; i++)
-	{
-		if (format[i] == '%')
-		{
-			size += get_fun(format[i + 1], args);
-			i += 1;
-		}
-		else
-		{
-			_putchar(format[i]);
-			size += 1;
-		}
-	}
-	va_end(args);
-	return (size);
+    if (format==NULL)
+        return (-1);
+
+    va_start(ap, format);
+    for (i = 0; format[i] != '\0'; i++)
+    {
+        if (format[i] == '%')
+        {
+            if (format[i + 1] == '\0')
+                return (-1);
+
+            else if (format[i + 1] == '%')
+            {
+                _putchar('%');
+                j++;
+                i++;
+            }
+            else if (print_all(format[i + 1]) != NULL)
+            {
+                j += (print_all(format[i + 1]))(ap);
+                i++;
+            }
+            else
+			{
+				_putchar(format[i]);
+				j++;
+			}
+        }
+        else
+        {
+            _putchar(format[i]);
+            j++;
+        }
+    }
+    va_end(ap);
+    return (j);
 }
